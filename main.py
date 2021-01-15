b79da7b53d995ca6ca73fe45898d0d9c04454bbc
Tatara <73353193+TataraXDD@users.noreply.github.com>
2021-01-15 22:53:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-hello
+import discord
+from discord.exe import commands
+import json
+
+
+with open {'setting.json'.'r'.encoding='utf8'} as jfile:
+    jdata = json.load(jfile)
+
+ bot = commands.Bot{commands_prefix='['}
+
+ @bot.event
+async def on_ready():
+    print("??連裕翔做愛了沒??")
+
+@bot.commanf()
+async def 圖片(ctx):
+    await ctx.send(' ㄚㄚㄚㄚ')
+
+bot.run(jdata['TOKEN'])
